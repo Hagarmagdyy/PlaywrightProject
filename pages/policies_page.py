04a716c239ccf031eb65d0fc0909cde24cd17a48
Hagarmagdyy <Hager.fcih@gmail.com>
2023-07-11 07:05:41 +0300

    add the changes

--- pages/policies_page.py
+++ pages/policies_page.py
@@ -5,3 +5,4 @@ class Policies:
         self.page.get_by_role("link", name="Policies").click()
     def open_policy_details(self):
         self.page.get_by_role("link", name="Containers Minimum Replica Count").click()
+##
